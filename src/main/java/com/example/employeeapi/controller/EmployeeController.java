1f7d20691fffaa3ea2bb4c0de28062ce4850d979
袁文琦 <1250761865@qq.com>
2018-04-21 06:27:56 -0400

    数据传输完成

--- src/main/java/com/example/employeeapi/controller/EmployeeController.java
+++ src/main/java/com/example/employeeapi/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.example.employeeapi.controller;
 
 import com.example.employeeapi.datasource.EmployeeRepository;
+import com.example.employeeapi.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @EnableAutoConfiguration
@@ -16,11 +23,23 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
 
     @RequestMapping("/employees")
-    String getEmployees(Model model) {
+    public ModelAndView getEmployees() {
 
-        model.addAttribute("message", "hello world");
-
-        return "employees";
+        List<Employee> es = employeeRepository.getAllEmployees();
+        List<Map<String,Object>> rs = new ArrayList<>();
+        for (Employee e:es){
+            Map<String, Object> map = new HashMap<>();
+            map.put("id", e.getId());
+            map.put("name", e.getName());
+            map.put("age", e.getAge());
+            map.put("gender", e.getGender());
+            rs.add(map);
+        }
+        ModelAndView mav = new ModelAndView("employees");
+//        model.addAttribute("message", "hello world");
+        mav.addObject("employees",rs);
+        mav.addObject("message","hello world");
+        return mav;
     }
 
 
@@ -28,4 +47,7 @@ public class EmployeeController {
     ResponseEntity getEmployee(@PathVariable("id") int id) {
         return new ResponseEntity<>(employeeRepository.getEmployee(id), HttpStatus.OK);
     }
+
+
+
 }
--- src/main/webapp/WEB-INF/jsp/employees.jsp
+++ src/main/webapp/WEB-INF/jsp/employees.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
          pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
 <html>
@@ -9,7 +11,19 @@
 </head>
 <body>
 
-<div>${message}</div>
+<table border="1">
+    <c:forEach items = "${employees}" var = "item">
+    <tr>
+    <td>${item.id}</td>
+    <td>${item.name}</td>
+    <td>${item.age}</td>
+    <td>${item.gender}</td>
+    </tr>
+    </c:forEach>
+</table>
+
+
+
 
 </body>
 </html>
